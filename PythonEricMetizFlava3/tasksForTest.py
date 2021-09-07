22c7e70df63f79777fd3fc8d4c3a8b6149d3f2c2
andrey-MS-7991 <you@example.com>
2021-09-07 09:46:52 +0300

    Initial commit

---
+++ PythonEricMetizFlava3/tasksForTest.py
@@ -0,0 +1,36 @@
+import json
+import requests
+
+
+def updateInfo():
+
+    fileName = "COMPANY.json"
+    url = "api/web/searchAbonents"
+
+    infoCompany = {
+   "companyContact": {
+        "displayName": "Company name",
+        "address": {
+            "streetAddress": "Company address"
+        },
+        "phoneNumbers": [
+            "company phone"
+        ]
+    },
+    "supportContact": {
+        "phoneNumber": "support phone",
+        "displayName": "Support",
+        "email": "support@email.com"
+    }
+}
+
+    requests.post(url, data=json.dumps(infoCompany))
+
+    infoList = [infoCompany]
+
+    with open(fileName, mode='w') as fObj:
+        json.dump(infoList, fObj)
+        fObj.close()
+
+
+updateInfo()
